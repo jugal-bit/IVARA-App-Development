f5bcb9bfc030a7519ec5e2923db1072c34c9baf5
sumit ojha <greatsojha88@gmail.com>
2020-11-17 17:23:56 +0530

    splash screen with timing

---
+++ lib/hometps.dart
@@ -0,0 +1,20 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:ivara_app/constant.dart';
+
+class Homescreen extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Scaffold(
+     /* body: Column(
+        children: <Widget[
+          Image(image: null)
+        ],
+      ), */
+    );
+      
+  }
+
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,90 +1,117 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:ivara_app/constant.dart';
+import 'package:ivara_app/hometps.dart';
 
 void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget{
+class MyApp extends StatefulWidget{
+
   @override
-  Widget build(BuildContext context) {
-    // TODO: implement build
-    return MaterialApp(
-      title: "IVARA APP",
-      theme: ThemeData(
-        primaryColor: primarycolor,
-      ),
-      home:  HomePage(),
-    );
-  }
-  
+  State<StatefulWidget> createState() => HomePage();
+
 }
 
+class HomePage extends State<MyApp>{
 
-class HomePage extends StatelessWidget{
+  @override
+  void initState() {
+    var d = Duration(seconds: 5);
+
+    Future.delayed(d,() {
+      Navigator.pushAndRemoveUntil(context,  MaterialPageRoute(builder: (context){
+        return Homescreen();
+      },
+      ),
+            (route) => false,
+      );
+    });
+    // TODO: implement initState
+    super.initState();
+
+
+  }
+
+
+
+
   @override
   Widget build(BuildContext context) {
+    // final size = MediaQuery.of(context).size;
     // TODO: implement build
-    return Scaffold(
+    return MaterialApp(
+      home: Scaffold(
       backgroundColor: Colors.black,
       body: Column(
         children: [
           Expanded(
             flex: 2,
-      child: Container(
-            decoration: BoxDecoration(
-              image: DecorationImage(image: AssetImage("assets/images/IVENTORS_INITIATIVES.png"),
+            child: Container(
+                decoration: BoxDecoration(
+                  image: DecorationImage(image: AssetImage("assets/images/IVENTORS_INITIATIVES.png"),
+                  ),
+                )
             ),
-          )
-          ),
           ),
           Expanded(
             flex: 1,
-              child: Column(
-                children: [
-                  RichText(
-                    textAlign: TextAlign.center,
-                      text: TextSpan(
-                        children: [
-                     //     TextSpan(
-                         //   text: "WELCOME\n",
-                     //       style: Theme.of(context).textTheme.headline2.copyWith(fontWeight: FontWeight.bold),
-                    //      ),
-                          TextSpan(
-                              text: "WELCOME\n\n",
-                              style: TextStyle(
-                                fontSize: 50,
-                                color: Colors.white,
-                              )
-                          ),
-                          TextSpan(
-                            text: "India's First Offline 3D & Interactive Learning through AR",
-                            style: TextStyle(
-                              fontSize: 14,
-
-                              color: Colors.white,
-                            ),
+            child: Column(
+              children: [
+                RichText(
+                  textAlign: TextAlign.center,
+                  text: TextSpan(
+                    children: [
+                      TextSpan(
+                          text: "WELCOME\n\n",
+                          style: TextStyle(
+                            fontSize: 50,
+                            color: Colors.white,
                           )
-                        ],
                       ),
-                  )
-                ],
-              ),
+                      TextSpan(
+                        text: "India's First Offline 3D & Interactive Learning through AR",
+                        style: TextStyle(
+                          fontSize: 14,
+
+                          color: Colors.white,
+                        ),
+                      )
+                    ],
+                  ),
+                )
+              ],
+            ),
           ),
           Expanded(flex: 1,
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              CircularProgressIndicator(),
-              Padding(padding: EdgeInsets.only(top: 20.0),
-              ),
-              Text("Loading",style: TextStyle(color: Colors.white,fontSize: 18.0,fontWeight: FontWeight.bold)),
-            ],
-          ),)
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                CircularProgressIndicator(),
+                Padding(padding: EdgeInsets.only(top: 20.0),
+                ),
+                Text("Loading",style: TextStyle(color: Colors.white,fontSize: 18.0,fontWeight: FontWeight.bold)),
+              ],
+            ),)
         ],
       ) ,
+      ),
     );
 
+
   }
-  
+
+   /* loaddata() async{
+    var duration = new Duration(seconds: 5);
+    return new Timer(duration,route);
+  }
+
+  route() async{
+    Navigator.pushReplacement(context, MaterialPageRoute(builder: (context)=>Homescreen()));
+
+  } */
+
 }
+
